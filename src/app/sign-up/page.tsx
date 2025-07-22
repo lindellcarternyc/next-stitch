9884ce39c133fe463a25a52fe3ee1a5b6188b64f
Lindell Carter <lindellcarter@Lindells-MacBook-Air.local>
2025-07-22 04:02:11 -0400

    remove duplicate input

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -37,17 +37,6 @@ export default function SignUpPage() {
       <div>
         <h1 className="text-2xl font-bold">Sign Up</h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-6 mt-4">
-          <div className="flex justify-between items-center">
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="Username"
-              className="w-full max-w-[80%] p-2 border rounded-sm"
-              required
-            />
-          </div>
           <Input
             label="Username"
             id="username"
