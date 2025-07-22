daf240909ab4000dfe504956a5922f397cd86b41
Lindell Carter <lindellcarter@Lindells-MacBook-Air.local>
2025-07-22 13:54:59 -0400

    creates actions

--- src/lib/actions.auth.ts
+++
@@ -1,7 +0,0 @@
-export const signin = async (formData: FormData) => {
-  "use server";
-  const username = formData.get("username");
-  const password = formData.get("password");
-
-  console.log({ username, password });
-};
---
+++ src/lib/actions.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import prisma from "./prisma";
+import { Section } from "@/generated/prisma";
+
+export const createProject = async (userId: string, projectName: string) => {
+  try {
+    await prisma.project.create({
+      data: {
+        userId,
+        name: projectName,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteProject = async (projectId: string) => {
+  try {
+    await prisma.project.delete({
+      where: { id: projectId },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const createSection = async (projectId: string, name: string) => {
+  try {
+    await prisma.section.create({
+      data: { projectId, name },
+    });
+    revalidatePath(`/projects/${projectId}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateSection = async (section: Section) => {
+  try {
+    await prisma.section.update({
+      where: {
+        id: section.id,
+      },
+      data: section,
+    });
+    revalidatePath(`/projects/${section.projectId}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteSection = async (id: string) => {
+  try {
+    await prisma.section.delete({ where: { id } });
+    revalidatePath(`/projects/${id}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
