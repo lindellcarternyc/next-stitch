bee74306daaf443cdacd29175d77e9e19218d83e
Lindell Carter <lindellcarter@Lindells-MacBook-Air.local>
2025-07-21 23:08:40 -0400

    updates links to projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toolbar } from "./(components)/toolbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
